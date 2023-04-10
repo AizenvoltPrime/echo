21ae27a60cd3c579825efdaf03779b1bbca6b1e4
Alex <alex11737@gmail.com>
2023-04-10 20:46:13 +0300

    hello world works

---
+++ app/src/main/java/com/example/echo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.echo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myTextView = findViewById<TextView>(R.id.textview)
+        myTextView.text = getString(R.string.my_string)
+    }
+}
