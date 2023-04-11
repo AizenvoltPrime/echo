722759549e0d4d780bccb649db3433cee857b63d
Alex <alex11737@gmail.com>
2023-04-11 14:02:16 +0300

    added firebase

--- app/src/main/java/com/example/echo/MainActivity.kt
+++ app/src/main/java/com/example/echo/MainActivity.kt
@@ -5,13 +5,11 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myTextView = findViewById<TextView>(R.id.textview)
         myTextView.text = getString(R.string.my_string)
-
     }
 }
