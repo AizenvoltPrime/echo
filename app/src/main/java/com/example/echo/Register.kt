3bdc0bb2bfbb6a8368feacecab5eb817b0241f96
Alex <alex11737@gmail.com>
2023-04-14 18:07:39 +0300

    working on registration

--- app/src/main/java/com/example/echo/Register.kt
+++ app/src/main/java/com/example/echo/Register.kt
@@ -2,14 +2,24 @@ package com.example.echo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.android.material.textfield.TextInputEditText
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+private const val TAG = "RegisterActivity"
 
 class Register : AppCompatActivity() {
 
     private lateinit var editTextEmail: TextInputEditText
     private lateinit var editTextPassword: TextInputEditText
     private lateinit var buttonReg: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +27,55 @@ class Register : AppCompatActivity() {
         editTextEmail = findViewById(R.id.email)
         editTextPassword = findViewById(R.id.password)
         buttonReg = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progressBar)
+        auth = FirebaseAuth.getInstance()
 
+        buttonReg.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Enter email",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Enter password",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Account created.",
+                            Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener(this) { exception ->
+                    // Handle the exception appropriately
+                    Log.w(TAG, "createUserWithEmail:failure", exception)
+                    Toast.makeText(this, "Authentication failed: ${exception.message}",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        // TODO: Update the user interface based on the current user
     }
 }
